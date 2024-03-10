fd1634af6cca41e602becbe8836fb261e9588400
samaddarmrinal <samaddarmrinal@gmail.com>
2024-03-11 00:27:01 +0530

    1492. The kth Factor of n

---
+++ src/main/java/com/example/leetcode/Q349_Intersection_of_two_Array.java
@@ -0,0 +1,39 @@
+package com.example.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Q349_Intersection_of_two_Array {
+
+   /* Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+
+
+            Example 1:
+
+    Input: nums1 = [1,2,2,1], nums2 = [2,2]
+    Output: [2]
+    Example 2:
+
+    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+    Output: [9,4]
+    Explanation: [4,9] is also accepted.
+
+
+            Constraints:
+
+            1 <= nums1.length, nums2.length <= 1000
+            0 <= nums1[i], nums2[i] <= 1000
+*/
+
+    class Solution {
+        public int[] intersection(int[] nums1, int[] nums2) {
+            return IntStream.of(nums2).distinct().filter(IntStream.of(nums1).boxed().collect(Collectors.toSet())::contains).toArray();
+
+
+        }
+    }
+}
